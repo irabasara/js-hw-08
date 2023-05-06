6672c76856a12b4978c372f132fb8454cf117fd5
irabasara <125157770+irabasara@users.noreply.github.com>
2023-05-06 23:13:13 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,19 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.getElementById('vimeo-player');
+
+const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
+const CURRENT_TIME_KEY = 'current-time'
 
 const currentTime = throttle((event) => {
     const currentTime = event.seconds;
-    localStorage.setItem('videoplayer-current-time', currentTime);
+    localStorage.setItem(CURRENT_TIME_KEY, currentTime);
 }, 1000);
 
 player.on('timeupdate', currentTime);
 
-const savedTime = localStorage.getItem('videoplayer-current-time');
+const savedTime = localStorage.getItem(CURRENT_TIME_KEY);
 if (savedTime) {
   player.setCurrentTime(parseFloat(savedTime));
 }
