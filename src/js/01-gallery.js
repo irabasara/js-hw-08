1432f1da98468c138b375ef83f2834cf891eefea
irabasara <125157770+irabasara@users.noreply.github.com>
2023-05-07 15:15:51 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -14,8 +14,8 @@ imgListRef.insertAdjacentHTML('beforeend', previewPhoto);
 function createPreviewPhoto(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
         return `<li class="gallery__item">
-   <a class="gallery__link" href=${original}>
-      <img class="gallery__image" src=${preview} alt=${description} />
+   <a class="gallery__link" href='${original}'>
+      <img class="gallery__image" src='${preview}' alt='${description}' />
    </a>
 </li>`}).join('');
 }
